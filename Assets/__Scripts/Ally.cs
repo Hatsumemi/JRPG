21dca33932d08941b0090a4231dcd887d4fb4a74
Hatsu <judithgarnier4@gmail.com>
2024-03-15 17:36:03 +0100

    Script choosing chara

--- Assets/__Scripts/Ally.cs
+++ Assets/__Scripts/Ally.cs
@@ -4,6 +4,38 @@ using UnityEngine;
 
 public class Ally : Character
 {
+    public GameObject f;
+    
+    string _visuChoosed01;
+    string _visuChoosed02;
+
+    private void Start()
+    {
+        Character visu = GetComponent<Character>();
+
+        if (gameObject.name == "Character01")
+        {
+            Transform _chara01 = transform.Find(_visuChoosed01);
+            SpriteRenderer _toShow01SR = _chara01.GetComponent<SpriteRenderer>();
+            Animator _toShow01A = _chara01.GetComponent<Animator>();
+            if (_toShow01SR.name == _visuChoosed01) 
+            {
+                visu.Visual = _toShow01SR;
+                visu.CharacterAnimator = _toShow01A;
+            }
+        }
+        if (gameObject.name == "Character02")
+        {
+            Transform _chara02 = transform.Find(_visuChoosed02);
+            SpriteRenderer _toShow02SR = _chara02.GetComponent<SpriteRenderer>();
+            Animator _toShow02A = _chara02.GetComponent<Animator>();
+            if (_toShow02SR.name == _visuChoosed02)
+            {
+                visu.Visual = _toShow02SR;
+                visu.CharacterAnimator = _toShow02A;
+            }
+        }
+    }
 
     internal override void Attack(Character defender)
     {
@@ -22,5 +54,15 @@ public class Ally : Character
         base.Hit(damage);
         CharacterAnimator.SetTrigger("hit");
         Life = Mathf.Clamp(Life - damage, 0, LifeMax);
+    }
+
+    private void LoadVisu()
+    {
+        _visuChoosed01 = PlayerPrefs.GetString("character01");
+    }
+
+    private void LoadAnim()
+    {
+        _visuChoosed01 = PlayerPrefs.GetString("character02");
     }
 }
